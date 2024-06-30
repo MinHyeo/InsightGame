8bcc2758a4401ea7b0165d76c9d96655eb13c324
TAK-C <taku@office.deu.ac.kr>
2024-06-30 13:00:59 +0900

    Animation
    24-06-30
    - 애니메이션 추가 및 수정

--- Insight_Game/Assets/Main/Scripts/Monster/Goblin/GoblinWalk.cs
+++ Insight_Game/Assets/Main/Scripts/Monster/Goblin/GoblinWalk.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Monster.Goblin
@@ -8,40 +6,43 @@ namespace Monster.Goblin
     {
         Rigidbody2D rigid;
         SpriteRenderer spriter;
+        Animator animator;
         public float Speed { private get; set; }
 
-        public int nextMove;
-        public int changeTime;
+        private int nextDir;
+        private int changeTime;
 
         private void Awake()
         {
             rigid = GetComponent<Rigidbody2D>();
             spriter = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
         }
-        private void Start() { ChangeDirection(); }
+        private void Start() 
+        { 
+            ChangeDirection();
+        }
 
         public void Walk()
         {
             spriter.flipX = rigid.velocity.x > 0f ? false : true;
-            rigid.velocity = new Vector2(nextMove*2, rigid.velocity.y);
+            rigid.velocity = new Vector2(nextDir*2, rigid.velocity.y);
 
-            Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.5f, rigid.position.y);
+            Vector2 frontVec = new Vector2(rigid.position.x + nextDir * 0.5f, rigid.position.y);
             RaycastHit2D groundInfo = Physics2D.Raycast(frontVec, Vector2.down, 3f);
+            if (groundInfo.collider == false) { nextDir *= -1; }
 
-            if (groundInfo.collider == false)
-            {
-                nextMove *= -1;
-            }
+            animator.SetFloat("Velocity", Mathf.Abs(rigid.velocity.x));
 
         }
-        void ChangeDirection()
+        private void ChangeDirection()
         {
             changeTime = Random.Range(1, 4);
-            nextMove = Normalize(Random.Range(-5, 5));
+            nextDir = Normalize(Random.Range(-5, 5));
             Invoke("ChangeDirection", changeTime);
         }
 
-        int Normalize(float value)
+        private int Normalize(float value)
         {
             if (value < -1.67f) return -1;
             else if (value > 1.67f) return 1;
